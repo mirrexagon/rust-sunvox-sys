079d301e4b29879c98beca6ceeea0e35afc5d1b2
Andrew Abbott <mirrexagon@mirrexagon.com>
2017-01-19 09:07:39 +1100

    Actually put shell.nix here

--- shell.nix
+++ shell.nix
@@ -1,1 +1,10 @@
-../shell.nix
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "rust-env";
+  buildInputs = [ stdenv pkgconfig openssl.dev cargo rustc alsaLib SDL binutils sunvox-dll ];
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+  SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+}
